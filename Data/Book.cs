2b0b6f5c6b894c5aad72e678225f863207a690f1
Keely Morris <keelymorris8@gmail.com>
2024-09-26 13:28:01 -0400

    update

---
+++ Data/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting_Algorithms.Data
+{
+    class Book : IComparable<Book>
+    {
+        public int CompareTo(Book? other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
